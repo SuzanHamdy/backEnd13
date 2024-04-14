c4136cfa17c97dc03fb29898d2f1d874091b05ed
SuzanHamdy <150383354+SuzanHamdy@users.noreply.github.com>
2024-04-14 23:02:38 +0200

    Add files via upload

--- user.js
+++ user.js
@@ -1,67 +1,55 @@
 
-const express = require('express')
-const User = require('../model/user')
-const router = express.Router()
- router.post('/users' ,(req , res) =>{
-    console.log(req.body)
-   const user = new User(req.body)
-  user.save()
-  .then((user) => { res.status(200).send(user)})
-  .catch((error) => { res.status(400).send(error)})
+const mongoose =require('mongoose')
+const validator =require ('validator')
+const { default: isEmail } = require('validator/lib/isEmail')
 
- })
-     router.get('/users' ,(req , res) => {
-      User.find({})
-      .then((user) =>{ res.status(200).send(user)})
-      .catch((error) => {res.status(500).send(error)})
-     })
 
-     router.get('/users/:id' ,(req , res) => {
-      const _id = req.params.id
-      User.findById(_id)
-      .then((user) =>{
-     if (!user) {
-      return   res.status(404).send('unable to find user')
-     }
-     res.status(200).send(user)
 
-      })
-      .catch((error) => { res.status(500).send(error)})
-     })
-////////////////////////////////////////////
-//patch
-router.patch('/users/:id' , async(req ,res) =>{
-   try{
-      const _id = req.params.id
-      const user = await User.findByIdAndUpdate(_id , req.body ,{
-         new : true ,
-         runValidators : true
-      })
-      if (!user) {
-         return   res.status(404).send('no user founded')
+const User = mongoose.model('User' , {
+    userName : {
+        type : String ,
+        required : true ,
+        trim : true
+    },
+    password :{
+        type : String ,
+        required : true ,
+        trim : true ,
+        minlength : 8 
+    } ,
+    email :{
+        type : String ,
+        required : true ,
+        trim : true ,
+        lowercase :true , 
+        unique : true ,
+        validate(value){
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is Invalid');
+            }
         }
-        res.status(200).send(user)
-   }
-   catch(error){ res.status(500).send(error)}
-})
-// //////////////////////////////////////////
-// //delete
-router.delete('/users/:id' , async(req , res) =>{
-   try{
-      const _id = req.params.id
-      const user =await User.findByIdAndDelete(_id)
-      if (!user) {
-         return res.status(404).send('no user founded')
-      }
-      res.status(200).send(user)
-   }
-   catch(e){ res.status(500).send(e)}
+    } ,
+    age : {
+        type : Number ,
+        default : 18 ,
+        validate(value){
+            if (value <= 0) {
+                throw new Error('Age isnot Postive');
+            }
+        }
+    } ,
+    city : {
+        type : String ,
+        
+    }
 })
 
+module.exports = User ;
 
 
 
-module.exports = router
+
+
 
 
 
